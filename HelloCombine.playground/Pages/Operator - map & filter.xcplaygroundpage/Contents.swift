f0363dc8ffa0d98da91ae36e6a69cc1432284590
최지연 <choi02647@naver.com>
2023-08-07 17:59:40 +0900

    Operator - map & filter

--- HelloCombine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ HelloCombine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -1,7 +1,33 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+// Transform
+let numPublisher = PassthroughSubject<Int, Never>()
 
-//: [Next](@next)
+let subscription1 = numPublisher
+    .map { $0 * 2 }
+    .sink { value in
+        print("Tranformed Value: \(value)")
+    }
+
+numPublisher.send(10)
+numPublisher.send(20)
+numPublisher.send(30)
+subscription1.cancel()
+
+
+// Filter
+let stringPublisher = PassthroughSubject<String, Never>()
+
+let subscription2 = stringPublisher
+    .filter { $0.contains("a") }
+    .sink { value in
+        print("Filtered Value: \(value)")
+    }
+
+stringPublisher.send("abc")
+stringPublisher.send("Jack")
+stringPublisher.send("Joon")
+stringPublisher.send("Jenny")
+stringPublisher.send("Jason")
+subscription2.cancel()
