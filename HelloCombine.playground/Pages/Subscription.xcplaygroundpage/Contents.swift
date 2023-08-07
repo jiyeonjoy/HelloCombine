90375d1366cdb87d50b9af8af0cdb16f081c6adc
최지연 <choi02647@naver.com>
2023-08-07 17:23:39 +0900

    Subscription

--- HelloCombine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ HelloCombine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -1,7 +1,47 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+let subject = PassthroughSubject<String, Never>()
 
-//: [Next](@next)
+// The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[Debug]")
+    .sink { (value) in
+        print("Subscriber received value: \(value)")
+    }
+//subject
+//    .print("[Debug]")
+//    .sink { (value) in
+//        print("Subscriber received value: \(value)")
+//    }
+//[Debug]: receive subscription: (PassthroughSubject)
+//[Debug]: request unlimited
+//[Debug]: receive value: (Hello!)
+//[Debug]: receive value: (Hello again!)
+//[Debug]: receive value: (Hello for the last time!)
+//[Debug]: receive finished
+
+subject.send("Hello!")
+subject.send("Hello again!")
+subject.send("Hello for the last time!")
+subject.send(completion: .finished) // 구독취소
+//subscription.cancel() // 구독취소
+subject.send("Hello?? :(")
+
+//Subscriber received value: Hello!
+//Subscriber received value: Hello again!
+//Subscriber received value: Hello for the last time!
+
+
+
+
+//[Debug]: receive subscription: (PassthroughSubject)
+//[Debug]: request unlimited
+//[Debug]: receive value: (Hello!)
+//Subscriber received value: Hello!
+//[Debug]: receive value: (Hello again!)
+//Subscriber received value: Hello again!
+//[Debug]: receive value: (Hello for the last time!)
+//Subscriber received value: Hello for the last time!
+//[Debug]: receive finished
+
