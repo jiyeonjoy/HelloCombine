b5f0c05bbdb2115a5819baf4f1d89dd9bfcd1d3c
최지연 <choi02647@naver.com>
2023-08-07 17:23:52 +0900

    Published

--- HelloCombine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ HelloCombine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+final class SomeViewModel {
+    @Published var name: String = "Jack" // 퍼블리셔
+    var age: Int = 20
+}
 
-//: [Next](@next)
+final class Label {
+    var text: String = ""
+}
+
+let label = Label()
+let vm = SomeViewModel()
+
+print("text: \(label.text)")
+
+// $name 퍼블리셔 접근 가능
+// assign: Publisher가 제공한 데이터를 특정 객체의 키패스에 할당
+vm.$name.assign(to: \.text, on: label)
+print("text: \(label.text)")
+
+vm.name = "Jason"
+print("text: \(label.text)")
+
+vm.name = "Hoo"
+print("text: \(label.text)")
+
+//text:
+//text: Jack
+//text: Jason
+//text: Hoo
