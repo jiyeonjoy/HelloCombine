37149cfd67a4b158a3f52520b7d6c6235cf6e1a2
최지연 <choi02647@naver.com>
2023-08-07 17:57:40 +0900

    Scheduler

--- HelloCombine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ HelloCombine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+let arrPublisher = [1, 2, 3].publisher
+
+let queue = DispatchQueue(label: "custom")
+
+let subscription = arrPublisher
+    .subscribe(on: queue) // 헤비한 작업인 경우 별도의 스레드에서 작업할 수있도록 할 수 있다.
+    .map { value -> Int in
+        print("transform: \(value), thread:\(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main) // 유아이 작업인 경우 받은 데이터를 메인 스레드에서 처리할 수 있도록 한다.
+    .sink { value in
+        print("Receive Value:\(value), thread:\(Thread.current)")
+    }
+//transform: 1, thread:<NSThread: 0x6000033847c0>{number = 7, name = (null)} // number = 7
+//transform: 2, thread:<NSThread: 0x6000033847c0>{number = 7, name = (null)}
+//transform: 3, thread:<NSThread: 0x6000033847c0>{number = 7, name = (null)}
+//Receive Value:1, thread:<_NSMainThread: 0x6000033ac0c0>{number = 1, name = main} // number = 1 => 메인 스레드 
+//Receive Value:2, thread:<_NSMainThread: 0x6000033ac0c0>{number = 1, name = main}
+//Receive Value:3, thread:<_NSMainThread: 0x6000033ac0c0>{number = 1, name = main}
